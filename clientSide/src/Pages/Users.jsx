2343120e1b50850ecc01ae374401c7e81cb14590
Masum-Reza <careerhunter4280@gmail.com>
2023-10-16 14:57:05 -0700

    user CRUD operation completed

--- clientSide/src/Pages/Users.jsx
+++ clientSide/src/Pages/Users.jsx
@@ -1,7 +1,55 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom"
+import Swal from "sweetalert2"
 
 const Users = () => {
-    const users = useLoaderData()
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers)
+
+    const handleDelete = (id) => {
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/user/${id}`, {
+                    method: 'DELETE',
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data?.acknowledged) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your user has been deleted.',
+                                'success'
+                            )
+                            let remaining = users?.filter(user => user?._id !== id);
+                            setUsers(remaining);
+                        }
+                        else {
+                            Swal.fire(
+                                'Something went wrong'
+                            )
+                        }
+                    })
+            }
+            else {
+                Swal.fire(
+                    'Cancelled!',
+                    'Your user is safe.',
+                    'error'
+                )
+            }
+        })
+    }
+
     return (
         <div>
             <div className="overflow-x-auto">
@@ -26,7 +74,7 @@ const Users = () => {
                                 <td>{user.userVerified ? 'Verified use' : 'Not verified'}</td>
                                 <td>{user.createdAt}</td>
                                 <td>{user.lastLoginTime}</td>
-                                <button className="btn btn-sm">X</button>
+                                <td className="btn btn-sm" onClick={() => handleDelete(user?._id)}>X</td>
                             </tr>)
                         }
                     </tbody>
